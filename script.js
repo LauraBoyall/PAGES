d7e1b2776b66ef49d3dcd336e015c72f56307afa
LauraBoyall <108723736+LauraBoyall@users.noreply.github.com>
2024-02-06 17:04:03 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.getElementById("birthdayModal");
+    var span = document.getElementsByClassName("close-button")[0];
+
+    // Show the modal
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
